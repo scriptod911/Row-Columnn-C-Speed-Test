6c164a6035686bf70354483566977d8747725029
Abdo Zain <abdo.zain.2008@gmail.com>
2023-06-08 01:10:47 +0300

    Implement Row-Major Traversal
    Update the main.c file and implement the traverseRowMajor function to traverse the array in row-major order

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
-#define ROWS 1000
-#define COLS 1000
+#define ROWS 3
+#define COLS 4
+
+void traverseRowMajor(int arr[ROWS][COLS]) {
+    int i, j;
+
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            printf("%d ", arr[i][j]);
+        }
+    }
+}
 
 int main() {
-    int arr[ROWS][COLS];
+    int arr[ROWS][COLS] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
 
+    traverseRowMajor(arr);
 
+    return 0;
 }
