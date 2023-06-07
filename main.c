3d0a9ea9215c8f770cb079833144667079ee1e2e
Abdo Zain <abdo.zain.2008@gmail.com>
2023-06-08 01:16:17 +0300

    Measure Time Difference
    Implemented code to measure and compare the time taken for row-major and column-major traversals of the multi-dimensional array. The clock function is used to record the start and end times of each traversal, and the difference in time is calculated and stored.

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h> // to test speed (obv)
 
 #define ROWS 3
 #define COLS 4
@@ -8,7 +10,7 @@ void traverseRowMajor(int arr[ROWS][COLS]) {
 
     for (i = 0; i < ROWS; i++) {
         for (j = 0; j < COLS; j++) {
-            printf("%d ", arr[i][j]);
+            arr[i][j] += 1; // prevent loop op
         }
     }
 }
@@ -18,7 +20,7 @@ void traverseColumnMajor(int arr[ROWS][COLS]) {
 
     for (j = 0; j < COLS; j++) {
         for (i = 0; i < ROWS; i++) {
-            printf("%d ", arr[i][j]);
+            arr[i][j] += 1; // prevent loop op
         }
     }
 }
@@ -30,11 +32,19 @@ int main() {
         {9, 10, 11, 12}
     };
 
-    printf("Row-Major Traversal:\n");
+    clock_t start, end;
+    double rowMajorTime, columnMajorTime;
+
+    start = clock();
     traverseRowMajor(arr);
+    end = clock();
+    rowMajorTime = (double)(end - start) / CLOCKS_PER_SEC;
 
-    printf("\n\n");
-
-    printf("Column-Major Traversal:\n");
+    start = clock();
     traverseColumnMajor(arr);
+    end = clock();
+    columnMajorTime = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("Time taken for row-major order: %.6f seconds\n", rowMajorTime);
+    printf("Time taken for column-major order: %.6f seconds\n", columnMajorTime);
 }
