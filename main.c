ea3c36629ef79e8874ac80a11d1d8956b7e57c4e
Abdo Zain <abdo.zain.2008@gmail.com>
2023-06-08 01:22:45 +0300

    Enhance Flexibility and User Input
    Implemented user input for array dimensions and value range, providing more flexibility to the program. The user is prompted to enter the number of rows, number of columns, minimum value, and maximum value for array initialization.

--- main.c
+++ main.c
@@ -1,49 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> // to test speed (obv)
+#include <time.h>
 
-#define ROWS 3
-#define COLS 4
-
-void traverseRowMajor(int arr[ROWS][COLS]) {
+void traverseRowMajor(int rows, int cols, int arr[rows][cols]) {
     int i, j;
 
-    for (i = 0; i < ROWS; i++) {
-        for (j = 0; j < COLS; j++) {
-            arr[i][j] += 1; // prevent loop op
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            arr[i][j] += 1; // Dummy operation to prevent loop optimization
         }
     }
 }
 
-void traverseColumnMajor(int arr[ROWS][COLS]) {
+void traverseColumnMajor(int rows, int cols, int arr[rows][cols]) {
     int i, j;
 
-    for (j = 0; j < COLS; j++) {
-        for (i = 0; i < ROWS; i++) {
-            arr[i][j] += 1; // prevent loop op
+    for (j = 0; j < cols; j++) {
+        for (i = 0; i < rows; i++) {
+            arr[i][j] += 1; // Dummy operation to prevent loop optimization
         }
+    }
+}
+
+void initializeArray(int rows, int cols, int arr[rows][cols], int minVal, int maxVal) {
+    int i, j;
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            arr[i][j] = (rand() % (maxVal - minVal + 1)) + minVal;
+        }
+    }
+}
+
+void printArray(int rows, int cols, int arr[rows][cols]) {
+    int i, j;
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
     }
 }
 
 int main() {
-    int arr[ROWS][COLS] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12}
-    };
+    srand(time(NULL));
+
+    int rows, cols;
+    int minVal, maxVal;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+    printf("Enter the minimum value for initialization: ");
+    scanf("%d", &minVal);
+    printf("Enter the maximum value for initialization: ");
+    scanf("%d", &maxVal);
+
+    int arr[rows][cols];
+
+    initializeArray(rows, cols, arr, minVal, maxVal);
+
+    printf("Original Array:\n");
+    printArray(rows, cols, arr);
 
     clock_t start, end;
     double rowMajorTime, columnMajorTime;
 
     start = clock();
-    traverseRowMajor(arr);
+    traverseRowMajor(rows, cols, arr);
     end = clock();
     rowMajorTime = (double)(end - start) / CLOCKS_PER_SEC;
 
     start = clock();
-    traverseColumnMajor(arr);
+    traverseColumnMajor(rows, cols, arr);
     end = clock();
     columnMajorTime = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("\n");
+
+    printf("Modified Array (Row-Major):\n");
+    printArray(rows, cols, arr);
+    printf("\n");
 
     printf("Time taken for row-major order: %.6f seconds\n", rowMajorTime);
     printf("Time taken for column-major order: %.6f seconds\n", columnMajorTime);
